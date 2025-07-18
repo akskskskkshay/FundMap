de4dac4f579a027970c518b4dc69662c074ab967
Akshay Sunil <sunilakshay653@gmail.com>
2025-07-18 22:34:21 +0530

    Add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Navbar } from "@/components"
+import { Analytics } from "@vercel/analytics/next"
 
 
 export const metadata: Metadata = {
