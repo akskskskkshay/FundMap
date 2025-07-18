0facea0fb4c77e1ad3508eebda829d4360f7dd0f
Akshay Sunil <sunilakshay653@gmail.com>
2025-07-18 22:47:59 +0530

    Add nextJs Config to use api driven images from randomuser

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'randomuser.me',
+        port: '',
+        pathname: '/api/portraits/**', // This makes it more secure by only allowing images from this path
+      },
+      // ... any other domains you need can go here
+    ],
+  },
 };
 
 export default nextConfig;
